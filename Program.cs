04cdc0c72910d97900cb1ea4477c3e364f61c190
Alona <122108963+yelloally@users.noreply.github.com>
2023-02-06 11:43:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace var_keyword
+{
+
+    class keywordVar
+    {
+
+        static void Main(string[] args)
+        {
+
+            var a = 120.23f;
+            var b = 150.23m;
+            var c = 'G';
+            var d = "yelloally";
+
+            // to print their type of variables
+            Console.WriteLine("value of c {0}, type {1}", a, a.GetType());
+            Console.WriteLine("value of d {0}, type {1}", b, b.GetType());
+            Console.WriteLine("value of e {0}, type {1}", c, c.GetType());
+            Console.WriteLine("value of f {0}, type {1}", d, d.GetType());
+
+            
+            Console.ReadLine();
+        }
+    }
+}
+
